38b6d2fbdfa0347033518302f77c2d19a89f12af
Zach Posten <Zach.Posten@jci.com>
2017-01-03 08:19:39 -0600

    Make colors reusable in both CSS and JavaScript
    See tutorial here: http://cek.io/blog/2016/06/30/postcss-color-vars/
    
    Colors are defined in src/utils/colors.js in CONSTANT_CASE, are
    transformed in webpack.config.js to --css-var-case for css.
    
    The variables are supplied to the CSS through postcss-cssnext.  In
    webpack.config.js they're added as a custom property.
    
    "ZACH_COLOR" => var(--zach-color) (css)
    "ZACH_COLOR" => require('./src/utils/colors').zach (js)

---
+++ src/utils/colors.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  "PRIMARY": "#2196F3",
+  "PRIMARY-LIGHT": "#64B5F6",
+  "PRIMARY-DARK": "#1565C0",
+  "NAV-COLOR": "#181818",
+  "NBV-LIGHT": "#282828",
+  "NAV-DARK": "#404040",
+  "DARK": "#282828",
+  "LIGHT": "#383838",
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,3 @@
-/**
- * React Static Boilerplate
- * https://github.com/kriasoft/react-static-boilerplate
- *
- * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-/* eslint-disable global-require */
-
 const path = require('path');
 const webpack = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
@@ -22,6 +10,17 @@ const babelConfig = Object.assign({}, pkg.babel, {
   babelrc: false,
   cacheDirectory: useHMR,
 });
+
+const R = require('ramda');
+// color vars in JS are CONST_CASE, but need to be converted to hyphen-case for CSS
+const renameKeys = R.curry((renameFn, obj) => {
+  return R.reduce((acc, key) => {
+    acc[renameFn(key)] = obj[key]
+    return acc
+  }, {}, R.keys(obj))
+})
+const constCaseToHyphenCase = (str) => { return str.replace(/_/g, "-").toLowerCase() }
+const colorVars = renameKeys(constCaseToHyphenCase, require('./src/utils/colors'));
 
 // Webpack configuration (main.js => public/dist/main.{hash}.js)
 // http://webpack.github.io/docs/configuration.html
@@ -148,45 +147,13 @@ const config = {
       // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
       // https://github.com/postcss/postcss-import
       require('postcss-import')({ addDependencyTo: bundler }),
-      // W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
-      // https://github.com/postcss/postcss-custom-properties
-      require('postcss-custom-properties')(),
-      // W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
-      // https://github.com/postcss/postcss-custom-media
-      require('postcss-custom-media')(),
-      // CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
-      // https://github.com/postcss/postcss-media-minmax
-      require('postcss-media-minmax')(),
-      // W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
-      // https://github.com/postcss/postcss-custom-selectors
-      require('postcss-custom-selectors')(),
-      // W3C calc() function, e.g. div { height: calc(100px - 2em); }
-      // https://github.com/postcss/postcss-calc
-      require('postcss-calc')(),
-      // Allows you to nest one style rule inside another
-      // https://github.com/jonathantneal/postcss-nesting
-      require('postcss-nesting')(),
-      // W3C color() function, e.g. div { background: color(red alpha(90%)); }
-      // https://github.com/postcss/postcss-color-function
-      require('postcss-color-function')(),
-      // Convert CSS shorthand filters to SVG equivalent, e.g. .blur { filter: blur(4px); }
-      // https://github.com/iamvdo/pleeease-filters
-      require('pleeease-filters')(),
-      // Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
-      // https://github.com/robwierzbowski/node-pixrem
-      require('pixrem')(),
-      // W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
-      // https://github.com/postcss/postcss-selector-matches
-      require('postcss-selector-matches')(),
-      // Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
-      // https://github.com/postcss/postcss-selector-not
-      require('postcss-selector-not')(),
+      // Allow the use of modern CSS syntax without requiring browser support
+      // http://cssnext.io/features/
+      require('postcss-cssnext')({features: {customProperties: {variables: colorVars}}}),
+      require('postcss-url')(),
       // Postcss flexbox bug fixer
       // https://github.com/luisrudge/postcss-flexbugs-fixes
       require('postcss-flexbugs-fixes')(),
-      // Add vendor prefixes to CSS rules using values from caniuse.com
-      // https://github.com/postcss/autoprefixer
-      require('autoprefixer')(),
     ];
   },
 
