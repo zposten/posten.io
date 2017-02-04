4e202625b0599d8f5e52a3f9d6ac360f7088f2f8
Zach Posten <zach@posten.io>
2017-02-03 21:10:47 -0600

    Factored out lightbox to own class

--- src/pages/photography/index.js
+++ src/pages/photography/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import Photoswipe from 'photoswipe'
-import PhotoswipeUI_Default from './photoswipe-ui-default'
+import PhotoswipeUI_Default from './photoswipe/photoswipe-ui-default'
+import Lightbox from './photoswipe/Lightbox.jsx'
 import Photos from './photos'
 import s from './styles.css'
 
@@ -19,42 +20,7 @@ export default class PhotographyPage extends Component {
 
   render() {
     return (
-      <div className="pswp" tabIndex="-1" role="dialog">
-        <div className="pswp__bg"></div>
-        <div className="pswp__scroll-wrap">
-          <div className="pswp__container">
-            <div className="pswp__item"></div>
-            <div className="pswp__item"></div>
-            <div className="pswp__item"></div>
-          </div>
-          <div className="pswp__ui pswp__ui--hidden">
-            <div className="pswp__top-bar">
-              <div className="pswp__counter"></div>
-              <button className="pswp__button pswp__button--close" title="Close (Esc)"></button>
-              <button className="pswp__button pswp__button--share" title="Share"></button>
-              <button className="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-              <button className="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-              <div className="pswp__preloader">
-                <div className="pswp__preloader__icn">
-                  <div className="pswp__preloader__cut">
-                    <div className="pswp__preloader__donut"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-              <div className="pswp__share-tooltip"></div>
-            </div>
-            <button className="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-            </button>
-            <button className="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-            </button>
-            <div className="pswp__caption">
-              <div className="pswp__caption__center"></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Lightbox />
     );
   }
 
