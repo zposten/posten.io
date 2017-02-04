fcd7ca8cc832f977e4970c42e078c3752ddd5e71
Zach Posten <zach@posten.io>
2017-02-03 21:08:38 -0600

    Photoswipe lightbox displaying

--- src/pages/photography/photoswipe/lightbox.jsx
+++ src/pages/photography/photoswipe/lightbox.jsx
@@ -1,21 +1,6 @@
 import React, { Component, PropTypes } from 'react'
-import Photoswipe from 'photoswipe'
-import PhotoswipeUI_Default from './photoswipe-ui-default'
-import Photos from './photos'
-import s from './styles.css'
 
-export default class PhotographyPage extends Component {
-
-  static propTypes = {
-  };
-
-  componentDidMount() {
-    document.title = "Photos";
-
-    let pswpElement = document.querySelectorAll('.pswp')[0];
-    let gallery = new Photoswipe(pswpElement, PhotoswipeUI_Default, Photos);
-    gallery.init()
-  }
+export default class Lightbox extends Component {
 
   render() {
     return (
