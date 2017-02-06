d5641e149d502375289c52a898916bc7f660e547
Zach Posten <zach@posten.io>
2017-02-05 20:49:25 -0600

    All of site seems to be working correctly

--- src/pages/bio/index.js
+++ src/pages/bio/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import cx from 'classnames'
 import { title, subtitle, html } from './bio.md'
-import markdownStyle from '../../utils/markdown.css'
+import s from './bio-style.css'
+import mds from '../../utils/markdown.css'
 
 export default class Bio extends Component {
 
@@ -11,7 +12,7 @@ export default class Bio extends Component {
 
   render() {
     return (
-      <div className={cx(s.content, markdownStyle.markdown)}>
+      <div className={cx(s.content, mds.markdown)}>
         <h1>{title}</h1>
         <h3>{subtitle}</h3>
         <div dangerouslySetInnerHTML={{ __html: html }} />
