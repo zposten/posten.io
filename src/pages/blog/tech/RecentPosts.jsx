a35b8eb2f6fb11719a009f0a08f7d3c1900ff7cc
Zach Posten <Zach.Posten@jci.com>
2017-02-19 16:35:45 -0600

    Factor out blog post parsing to separate file

--- src/pages/blog/tech/RecentPosts.jsx
+++ src/pages/blog/tech/RecentPosts.jsx
@@ -3,44 +3,11 @@ import cx from 'classnames'
 import s from './RecentPosts.css'
 
 import Tile from './BlogTile.jsx'
+import posts from './posts/posts'
 
-import * as iosNav from './posts/ios-nav-controller.md'
-import * as iosBreak from './posts/ios-required-break-statements.md'
-import * as iosStatic from './posts/ios-storing-static-data.md'
-import * as iosUiCreation from './posts/ios-ui-creation-methods.md'
-import * as iosPopover from './posts/ios-ui-popover-controller.md'
-import * as iosVarCopy from './posts/ios-view-var-copy.md'
 
 export default class RecentPosts extends Component {
-  splitTags(tagstr) {
-    if (!tagstr) return [];
-    return tagstr.split(',').map(tag => { if(tag) return tag.trim() });
-  }
-
-  parseDate(md) {
-    if (!md.date) return;
-    let monthStrs = ['January', 'February', 'March', 'April', 'May',
-                     'June', 'July', 'August', 'September', 'October',
-                     'November', 'December'];
-    let d = md.date.split('-');
-
-    md.dateObject = new Date(d[0], d[1], d[2]);
-    md.dateString = `${monthStrs[d[1]]} ${d[2]}, ${d[0]}`;
-  }
-
   render() {
-    let posts = [iosNav, iosBreak, iosStatic, iosUiCreation, iosPopover, iosVarCopy];
-    for (let post of posts) this.parseDate(post);
-
-    posts.sort(function (md1, md2) {
-      let date1 = md1.dateObject;
-      let date2 = md2.dateObject;
-
-      if (date1 < date2) return 1;
-      if (date1 > date2) return -1;
-      return 0;
-    });
-
     let tiles = posts.map(function(post, index) {
       return (
         <Tile title={post.title}
@@ -48,7 +15,7 @@ export default class RecentPosts extends Component {
               author={post.author}
               summary={post.summary}
               key={post.title}
-              tags={this.splitTags(post.tags)}/>
+              tags={post.tags}/>
       );
     }, this);
 
