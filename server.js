880bc2163480002f051ee226d77a0d5386dec9ad
Zach Posten <Zach.Posten@jci.com>
2017-02-25 16:30:28 -0600

    Add port for production

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var devMiddleware = require('webpack-dev-middleware');
 var hotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
 
+var env = process.env.NODE_ENV || 'development';
+
 var app = express();
 var compiler = webpack(config);
 
@@ -20,10 +22,11 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(3000, function (err) {
+var port = env == 'production' ? process.env.port : 3000;
+app.listen(port, function (err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:' + port + '/');
 });
